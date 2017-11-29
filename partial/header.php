f9dc0b74815dae9ae63a610ff2dd4cae3cf84eca
raph5mrn <raphamim@hotmail.com>
2017-11-29 15:35:32 +0100

    finition header

--- partial/header.php
+++ partial/header.php
@@ -21,33 +21,33 @@
             </button> -->
             <div class="nav-right" id="navbarResponsive">
             <ul>
-                <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                <li>
+                <a href="#about">About</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+                <li>
+                <a href="#services">Services</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+                <li>
+                <a href="#portfolio">Portfolio</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                <li>
+                <a href="#contact">Contact</a>
                 </li>
             </ul>
             </div>
         </div>
         </nav>
 
-      <div class="container my-auto">
+      <div class="container">
         <div class="row">
-          <div class="col-lg-10 mx-auto">
-            <h1 class="text-uppercase">
+          <div>
+            <h1>
               <strong>Your Favorite Source of Free Bootstrap Themes</strong>
             </h1>
             <hr>
           </div>
-          <div class="col-lg-8 mx-auto">
-            <p class="text-faded mb-5">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+          <div>
+            <p class="text-header">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
             <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
           </div>
         </div>
