8debe502e6e679a77b8dd7579b26aa0bff2acd95
raph5mrn <raphamim@hotmail.com>
2017-11-29 20:33:31 +0100

    portfolio part

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php include 'partial/header.php'; ?>
 
     <main>
-    <section class="bg-primary" id="about">
+    <section id="about">
       <div class="container">
         <div class="row">
-          <div class="col-lg-8 mx-auto text-center">
-            <h2 class="section-heading text-white">We've got what you need!</h2>
-            <hr class="light my-4">
-            <p class="text-faded mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
-            <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+          <div class="text-center">
+            <h2>We've got what you need!</h2>
+            <hr>
+            <p>Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
+            <a class="btn" href="#services">Get Started!</a>
           </div>
         </div>
       </div>
@@ -17,40 +17,40 @@
     <section id="services">
       <div class="container">
         <div class="row">
-          <div class="col-lg-12 text-center">
-            <h2 class="section-heading">At Your Service</h2>
-            <hr class="my-4">
+          <div class="text-center">
+            <h2>At Your Service</h2>
+            <hr>
           </div>
         </div>
       </div>
       <div class="container">
         <div class="row">
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
+          <div class="col-25">
+            <div>
               <i class="fa fa-4x fa-diamond text-primary mb-3 sr-icons"></i>
-              <h3 class="mb-3">Sturdy Templates</h3>
-              <p class="text-muted mb-0">Our templates are updated regularly so they don't break.</p>
+              <h3>Sturdy Templates</h3>
+              <p>Our templates are updated regularly so they don't break.</p>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
+          <div class="col-25">
+            <div>
               <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
-              <h3 class="mb-3">Ready to Ship</h3>
-              <p class="text-muted mb-0">You can use this theme as is, or you can make changes!</p>
+              <h3>Ready to Ship</h3>
+              <p>You can use this theme as is, or you can make changes!</p>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
+          <div class="col-25">
+            <div>
               <i class="fa fa-4x fa-newspaper-o text-primary mb-3 sr-icons"></i>
-              <h3 class="mb-3">Up to Date</h3>
-              <p class="text-muted mb-0">We update dependencies to keep things fresh.</p>
+              <h3>Up to Date</h3>
+              <p>We update dependencies to keep things fresh.</p>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
+          <div class="col-25">
+            <div>
               <i class="fa fa-4x fa-heart text-primary mb-3 sr-icons"></i>
-              <h3 class="mb-3">Made with Love</h3>
-              <p class="text-muted mb-0">You have to make your websites with love these days!</p>
+              <h3>Made with Love</h3>
+              <p>You have to make your websites with love these days!</p>
             </div>
           </div>
         </div>
@@ -58,9 +58,9 @@
     </section>
 
     <section class="p-0" id="portfolio">
-      <div class="container-fluid p-0">
-        <div class="row no-gutters popup-gallery">
-          <div class="col-lg-4 col-sm-6">
+      <div class="container-fluid">
+        <div class="row">
+          <div>
             <a class="portfolio-box" href="asset-image/1.jpg">
               <img class="img-fluid" src="asset-image/1.jpg" alt="image avec texte en hover">
               <div class="portfolio-box-caption">
@@ -75,7 +75,7 @@
               </div>
             </a>
           </div>
-          <div class="col-lg-4 col-sm-6">
+          <div>
             <a class="portfolio-box" href="asset-image/2.jpg">
               <img class="img-fluid" src="asset-image/2.jpg" alt="image avec texte en hover">
               <div class="portfolio-box-caption">
@@ -90,7 +90,7 @@
               </div>
             </a>
           </div>
-          <div class="col-lg-4 col-sm-6">
+          <div>
             <a class="portfolio-box" href="asset-image/3.jpg">
               <img class="img-fluid" src="asset-image/3.jpg" alt="image avec texte en hover">
               <div class="portfolio-box-caption">
@@ -105,7 +105,7 @@
               </div>
             </a>
           </div>
-          <div class="col-lg-4 col-sm-6">
+          <div>
             <a class="portfolio-box" href="asset-image/4.jpg">
               <img class="img-fluid" src="asset-image/4.jpg" alt="image avec texte en hover">
               <div class="portfolio-box-caption">
@@ -120,7 +120,7 @@
               </div>
             </a>
           </div>
-          <div class="col-lg-4 col-sm-6">
+          <div>
             <a class="portfolio-box" href="asset-image/5.jpg">
               <img class="img-fluid" src="asset-image/5.jpg" alt="image avec texte en hover">
               <div class="portfolio-box-caption">
@@ -135,7 +135,7 @@
               </div>
             </a>
           </div>
-          <div class="col-lg-4 col-sm-6">
+          <div>
             <a class="portfolio-box" href="asset-image/6.jpg">
               <img class="img-fluid" src="asset-image/6.jpg" alt="image avec texte en hover">
               <div class="portfolio-box-caption">
